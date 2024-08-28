99adf4e5e9fb2b1f7ac48e2d74d8258f5cb73672
Yunsik Han <yunsikhan.yh@gmail.com>
2024-08-29 10:17:34 +1200

    Finished prototype and challenge

---
+++ Prototype2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+
+    private void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
--- Prototype2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
@@ -22,6 +22,7 @@ public class DestroyOut : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
